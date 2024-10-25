9b264b7acb59e68f540c763ea5503a50ac04a452
Jagbir Singh <singhjagbirg171@gmail.com             git config user.name jagbirsingh09>
2024-10-26 01:41:49 +0530

    commit

---
+++ script.js
@@ -0,0 +1,52 @@
+var cart = [];
+
+function addToCart(productName, productPrice) {
+    var existingProduct = null;
+
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i].name === productName) {
+            existingProduct = cart[i];
+            break;
+        }
+    }
+
+    if (!existingProduct) {
+        cart.push({ name: productName, price: productPrice });
+        alert("Product added to Cart: " + productName);
+        updateCartDisplay();
+    } else {
+        alert(productName + " is already in the cart.");
+    }
+}
+
+function updateCartDisplay() {
+    var cartContainer = document.getElementById('cart-items');
+    var totalElement = document.getElementById('total');
+    
+    cartContainer.innerHTML = '';
+    var totalPrice = 0;
+
+    for (var i = 0; i < cart.length; i++) {
+        var item = document.createElement('div');
+        item.textContent = cart[i].name + ' - ₹' + cart[i].price;
+        cartContainer.appendChild(item);
+        totalPrice += cart[i].price;
+    }
+
+    totalElement.textContent = 'Total: ₹' + totalPrice;
+}
+
+
+
+function continueShopping() {
+    window.location.href = 'products.html'; 
+}
+
+var checkoutBtn = document.getElementById('checkout-btn');
+if (checkoutBtn) {
+    checkoutBtn.addEventListener('click', function() {
+        alert('Thank you for your order!');
+        cart = [];
+        updateCartDisplay();
+    });
+}
